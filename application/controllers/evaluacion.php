afed0afbd3571eae039bc25f7946af29afad342a
Vicecalidadumb <yeison.arias@umb.edu.co>
2015-01-13 14:29:37 -0500

    Cambio index evaluacion
    Cambio index evaluacion

--- application/controllers/evaluacion.php
+++ application/controllers/evaluacion.php
@@ -16,52 +16,60 @@ class Evaluacion extends CI_Controller {
     public function index() {
         //FUNCION PRINCIPAL PARA EL LOGIN - CARGA LA VISTA LOGIN/INDEX.PHP           
         $data['title'] = '';
-        $data['get']=  $this->input->get();
-        if($data['get']['id']){
-        $data['get']['id']=deencrypt_id($data['get']['id']);
-        $data['datos']= $this->evaluacion_model->datos_personales($data['get']['id']);
-        if(empty($data['datos'])){
-            redirect('index.php/login', 'location');
-        }
-        $data['documentos']= $this->evaluacion_model->documentos($data['get']['id']);
-        $data['educacion_formal']= $this->evaluacion_model->educacion_formal($data['get']['id']);
-        $data['content'] = 'evaluacion/educacion_formal';
-        $data['educacion_formal']=$this->load->view('evaluacion/educacion_formal_titulo', $data,true);
-        $this->load->view('template/template', $data);
-//        $this->load->view('evaluacion/educacion_formal', $data);
-        }else{
+        $data['get'] = $this->input->get();
+        if ($data['get']['id']) {
+            $data['get']['id'] = deencrypt_id($data['get']['id']);
+            $data['datos'] = $this->evaluacion_model->datos_personales($data['get']['id']);
+            if (empty($data['datos']))
+                redirect('index.php/login', 'location');
+            //DATOS USUARIO
+            $datos['documentos'] = $this->evaluacion_model->documentos($data['get']['id']);
+            $datos['educacion_formal'] = $this->evaluacion_model->educacion_formal($data['get']['id']);
+            $datos['experiencia'] = $this->evaluacion_model->educacion_formal($data['get']['id']);
+            //VISTAS DOCUMENTOS
+            $data['doc_espeficifos'] = $this->load->view('evaluacion/documentos/especificos', $datos, true);
+            $data['doc_educacion'] = $this->load->view('evaluacion/documentos/educacion', $datos, true);
+            $data['doc_experiencia'] = $this->load->view('evaluacion/documentos/experiencia', $datos, true);
+            $data['cumple'] = $this->load->view('evaluacion/documentos/cumple', $datos, true);
+            //VISTA GENERAL
+            $data['content'] = 'evaluacion/educacion_formal';
+            $this->load->view('template/template', $data);
+        } else {
             redirect('index.php/login', 'location');
         }
     }
 
     public function calificar_modalidad() {
         $data['post'] = $this->input->post();
-        $data['modalidad']= get_dropdown($this->evaluacion_model->modalidad(),'IDMODALIDAD_MOD','MODALIDAD_MOD');
+        $data['modalidad'] = get_dropdown($this->evaluacion_model->modalidad(), 'IDMODALIDAD_MOD', 'MODALIDAD_MOD');
         $this->load->view('evaluacion/calificar_modalidad', $data);
     }
+
     public function universidad() {
         $data['post'] = $this->input->post('universidad');
-        $dat= $this->evaluacion_model->universidad($data['post']);
-        $valu="<option value='0'>Seleccione</option>";
+        $dat = $this->evaluacion_model->universidad($data['post']);
+        $valu = "<option value='0'>Seleccione</option>";
         foreach ($dat as $value) {
-            $valu.="<option value='".$value->IDUNIVERSIDAD_UNIV."'>".$value->UNIVERSIDAD_UNIV."</option>";
+            $valu.="<option value='" . $value->IDUNIVERSIDAD_UNIV . "'>" . $value->UNIVERSIDAD_UNIV . "</option>";
         }
         echo $valu;
 //        echo $data['universidad']= $this->evaluacion_model->universidad($data['post']);
     }
+
     public function titulo() {
         $data['post'] = $this->input->post('titulo');
-        $dat= $this->evaluacion_model->titulo($data['post']);
-        $valu="<option value='0'>Seleccione</option>";
+        $dat = $this->evaluacion_model->titulo($data['post']);
+        $valu = "<option value='0'>Seleccione</option>";
         foreach ($dat as $value) {
-            $valu.="<option value='".$value->IDTITULO_TIT."'>".$value->TITULO_TIT."</option>";
+            $valu.="<option value='" . $value->IDTITULO_TIT . "'>" . $value->TITULO_TIT . "</option>";
         }
         echo $valu;
 //        echo $data['universidad']= $this->evaluacion_model->universidad($data['post']);
     }
+
     public function consultar_opec() {
         $data['post'] = $this->input->post();
-        $data['datos']= $this->evaluacion_model->datos_personales($data['post']['id']);
+        $data['datos'] = $this->evaluacion_model->datos_personales($data['post']['id']);
         $this->load->view('evaluacion/consultar_opec', $data);
     }
 
